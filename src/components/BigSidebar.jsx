f3075e2a1d38ae938787df0d5bdffb42ab637178
mattbuttondev <hello@mattbutton.dev>
2024-02-28 13:26:22 +0000

    feat: Add BigSidebar component
    This commit introduces the BigSidebar component. The BigSidebar component serves as the main navigation for large screens and is used within the SharedLayout component.

--- src/components/BigSidebar.jsx
+++ src/components/BigSidebar.jsx
@@ -1,9 +1,25 @@
+import { useSelector } from "react-redux";
 import Wrapper from "../assets/wrappers/BigSidebar";
+import Logo from "./Logo";
+import NavLinks from "./NavLinks";
 
 const BigSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
+
   return (
     <Wrapper>
-      <div>BigSidebar</div>
+      <div
+        className={
+          isSidebarOpen ? "sidebar-container" : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   );
 };
